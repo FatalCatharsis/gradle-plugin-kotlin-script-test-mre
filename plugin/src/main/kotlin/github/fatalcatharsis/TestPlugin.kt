62545d826a6521d475603fc1da1bcc51ae2d7fc0
Martin Bonnin <martin@mbonnin.net>
2023-07-23 23:42:47 +0200

    this seams to work

--- plugin/src/main/kotlin/github/fatalcatharsis/TestPlugin.kt
+++ plugin/src/main/kotlin/github/fatalcatharsis/TestPlugin.kt
@@ -4,8 +4,13 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.provider.Property
 import java.io.File
+import kotlin.script.experimental.api.KotlinType
 import kotlin.script.experimental.api.ScriptDiagnostic
+import kotlin.script.experimental.host.createCompilationConfigurationFromTemplate
+import kotlin.script.experimental.host.createEvaluationConfigurationFromTemplate
 import kotlin.script.experimental.host.toScriptSource
+import kotlin.script.experimental.host.withDefaultsFrom
+import kotlin.script.experimental.jvm.defaultJvmScriptingHostConfiguration
 import kotlin.script.experimental.jvmhost.BasicJvmScriptingHost
 import kotlin.script.experimental.jvmhost.createJvmCompilationConfigurationFromTemplate
 
@@ -16,7 +21,13 @@ open class TestPlugin : Plugin<Project> {
         project.tasks.create("test-kts-script") {
             it.doLast {
                 val extension = project.extensions.getByType(PluginExtension::class.java)
-                val configuration = createJvmCompilationConfigurationFromTemplate<ScriptDefinition> {  }
+                //val configuration = createJvmCompilationConfigurationFromTemplate<ScriptDefinition> {  }
+                val configuration = createCompilationConfigurationFromTemplate(
+                    KotlinType(ScriptDefinition::class),
+                    null.withDefaultsFrom(defaultJvmScriptingHostConfiguration),
+                    ScriptDefinition::class
+                )
+
                 val results = BasicJvmScriptingHost().eval(extension.file.get().toScriptSource(), configuration, null)
                 results.reports.forEach {
                     if (it.severity > ScriptDiagnostic.Severity.DEBUG) {
